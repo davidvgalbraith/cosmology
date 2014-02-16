9c3c21ec5dfab5e7220730be146f85a74150f454
David <david@david-VPCEB26GM.(none)>
2014-02-15 21:40:55 -0500

    New graph

--- universeage/hubdensity.py
+++ universeage/hubdensity.py
@@ -12,13 +12,13 @@ def agenolambdamgreaterthan1(omegaz):
     theta = math.acos(1.0 - 2.0 * (omegaz - 1) / omegaz)
     return 0.5 * omegaz / ((omegaz - 1)**1.5) * (theta - math.sin(theta))
 
-#Age of the universe with cosmological constant for omega_0m less than 1
-def agelambdaomegalessthan1(omegaz):
-    return 2.0 / (3.0 * math.sqrt(1 - omegaz)) * math.log((math.sqrt(1 - omegaz) + 1) / math.sqrt(omegaz))
+#Hubble constant at time t for mass density parameter omegaz less than 1
+def hlambdaomegalessthan1(omegaz, t):
+    return (1.0 / t) * 2.0 / (3.0 * math.sqrt(1 - omegaz)) * math.log((math.sqrt(1 - omegaz) + 1) / math.sqrt(omegaz))
 
-#Age of universe with cosmological constant for omega_0m greater than 1
-def agelambdaomegagreaterthan1(omegaz):
-    return 2.0 / (3.0 * math.sqrt(omegaz - 1)) * math.asin(math.sqrt((omegaz - 1) / omegaz))
+#Hubble constant at time t for mass density parameter omegaz for omega_0m greater than 1
+def hlambdaomegagreaterthan1(omegaz, t):
+    return (1.0 / t) * 2.0 / (3.0 * math.sqrt(omegaz - 1)) * math.asin(math.sqrt((omegaz - 1) / omegaz))
 
 #Age of the universe as a function of density parameter
 #in a universe without cosmological constant
@@ -32,20 +32,21 @@ def fa(x):
 #Age of the universe as a function of matter density parameter in 
 #a universe with cosmological constant constrained by
 #matter density parameter + lambda density parameter = 1
-def fb(x):
+def fb(x, t):
     if x < 1:
-        return agelambdaomegalessthan1(x)
+        return hlambdaomegalessthan1(x, t)
     if x > 1:
-        return agelambdaomegagreaterthan1(x)
-    return 0.5 * (fb(1.001) + fb(0.999))
+        return hlambdaomegagreaterthan1(x, t)
+    return 0.5 * (fb(1.001, t) + fb(0.999, t))
 
 x = np.arange(0.001, 3.0, 0.001)
-y1 = [fa(y) for y in x]
-y2 = [fb(y) for y in x]
+y1 = [fb(y, 11.5) for y in x]
+y2 = [fb(y, 13.8) for y in x]
+y3 = [fb(y, 18.0) for y in x]
 
-
-plt.plot(x, y1, label="Lambda = 0")
-plt.plot(x, y2, label="Lambda + m = 1")
+plt.plot(x, y1, label="t0 = 11.5")
+plt.plot(x, y2, label="t0 = 13.8")
+plt.plot(x, y3, label="t0 = 18")
 plt.xlabel("Density parameter", fontsize=30)
 plt.ylabel("Age of the Universe", fontsize=30)
 plt.title("Age of the Universe vs. Density parameter", fontsize=30)
